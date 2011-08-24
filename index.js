5b33417a33b1b06415ace254e9a222ded8e10957
Howard Abrams <howard@howardabrams.com>
2011-08-24 15:45:22 -0700

    Explained what this top-level file does.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+/*
+ * As you can tell, this doesn't do that much.
+ * It pulls in the modules, and starts the
+ * server with the router code, and the
+ * restops (REST Operations).
+ */
+
 var server  = require("./js/server");
 var router  = require("./js/router");
 var restops = require("./js/restops");
